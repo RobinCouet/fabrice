4bd32617ade9ab0c019bed96c8759537c0e3b9f1
Robin Couet Windows <robin@start-win.fr>
2026-08-17 14:46:26 +0200

    ajout crud animal

--- zoo/src/Controller/AnimalController.php
+++ zoo/src/Controller/AnimalController.php
@@ -4,9 +4,15 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 // Va me servir à récupérer des informations en base de donnée
 use App\Repository\AnimalRepository;
+// On recupère le model / entity
+use App\Entity\Animal;
+use App\Form\AnimalType;
+// Lui nous permet de sauvegarder, modifier, supprimer des infos en DB
+use Doctrine\ORM\EntityManagerInterface;
 
 final class AnimalController extends AbstractController
 {
@@ -19,6 +25,69 @@ final class AnimalController extends AbstractController
 
         return $this->render('animal/index.html.twig', [
             'animals' => $animals
+        ]);
+    }
+
+    #[Route("/animal/new", name: "app_animal_new")]
+    public function new(Request $request, EntityManagerInterface $em): Response
+    {
+        $animal = new Animal;
+        $form = $this->createForm(AnimalType::class, $animal);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($animal);
+            $em->flush();
+
+            return $this->redirectToRoute("app_animal_show", ['id' => $animal->getId()]);
+        }
+
+
+        return $this->render('animal/new.html.twig', [
+            'form' => $form
+        ]);
+    }
+
+    #[Route("/animal/{id}/edit", name: "app_animal_edit")]
+    public function edit(Request $request, EntityManagerInterface $em, Animal $animal): Response
+    {
+        $form = $this->createForm(AnimalType::class, $animal);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($animal);
+            $em->flush();
+
+            return $this->redirectToRoute("app_animal_show", ['id' => $animal->getId()]);
+        }
+
+
+        return $this->render('animal/edit.html.twig', [
+            'form' => $form,
+            'animal' => $animal
+        ]);
+    }
+
+    #[Route("/animal/{id}/delete", name: "app_animal_delete")]
+    public function delete(Animal $animal, EntityManagerInterface $em): Response
+    {
+        if ($animal) {
+            $em->remove($animal);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute("app_animal");
+    }
+
+    #[Route("/animal/{id}", name: "app_animal_show")]
+    public function show(Animal $animal): Response
+    {
+        if (!$animal) {
+            return $this->redirectToRoute('app_animal');
+        }
+
+        return $this->render('animal/show.html.twig', [
+            'animal' => $animal
         ]);
     }
 }
