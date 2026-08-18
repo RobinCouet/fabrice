fbb8b4b983f0562875e2f84d44fadc2f8d1e3014
Robin Couet Windows <robin@start-win.fr>
2026-08-18 14:39:33 +0200

    auth

--- zoo/src/Controller/PlayerController.php
+++ zoo/src/Controller/PlayerController.php
@@ -10,6 +10,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/player')]
 final class PlayerController extends AbstractController
@@ -22,6 +23,7 @@ final class PlayerController extends AbstractController
         ]);
     }
 
+    #[IsGranted("ROLE_USER")]
     #[Route('/new', name: 'app_player_new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
@@ -50,6 +52,7 @@ final class PlayerController extends AbstractController
         ]);
     }
 
+    #[IsGranted("ROLE_USER")]
     #[Route('/{id}/edit', name: 'app_player_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Player $player, EntityManagerInterface $entityManager): Response
     {
@@ -68,6 +71,7 @@ final class PlayerController extends AbstractController
         ]);
     }
 
+    #[IsGranted("ROLE_ADMIN")]
     #[Route('/{id}', name: 'app_player_delete', methods: ['POST'])]
     public function delete(Request $request, Player $player, EntityManagerInterface $entityManager): Response
     {
